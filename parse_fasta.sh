70c2631dc2e1bfa185c20e514e3aaa0e54787a47
Maggie Chen <ychen254@dons.usfca.edu>
2018-09-07 15:41:28 -0700

    set up directory sturcture

---
+++ parse_fasta.sh
@@ -0,0 +1,10 @@
+# Script to set up a project directory to print out the sequence names
+# Also to print out the sequence counts for each fasta files
+# Initially created by Maggie Chen and Matthew Mcgregor
+# on September 7,2018
+# ychen254@dons.usfca.edu mmcgregor2@dons.usfca.edu
+
+# Set up directory sturcture
+echo "setting up directory structure"
+echo "code data data/raw_data output"
+mkdir code data data/raw_data output
