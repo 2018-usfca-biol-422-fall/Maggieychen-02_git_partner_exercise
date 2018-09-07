4e4ee03a2613b4a5e615297249da0ef1bfe0ebae
Maggie Chen <ychen254@dons.usfca.edu>
2018-09-07 16:27:06 -0700

    print sequence name

--- parse_fasta.sh
+++ parse_fasta.sh
@@ -15,4 +15,16 @@ curl -L https://npk.io/NB6Y5a+ -o data/raw_data/sequence_data.zip
 
 #unzip the files
 echo "Unizping the files to data/raw_data directory..... "
-unzip data/raw_data/sequence_data.zip
+unzip data/raw_data/sequence_data.zip -d data/raw_data/
+
+# Pull out all of the sequence name from the fasta file
+# and put them them into seperate text file in the output folder
+echo "Extracting sequence name from each of the files"
+grep ">" data/raw_data/animal_mRNA.fasta > output/sequence_names_animal_mRNA.txt
+grep ">" data/raw_data/crab_mitochondrial_rRNA.fasta > output/sequence_names_crab_rRNA.txt
+grep ">" data/raw_data/invertebrates_mRNA.fasta > output/sequence_names_invertebrates_mRNA.txt
+grep ">" data/raw_data/plant_rRNA.fasta > output/sequence_names_plant_rRNA.txt
+
+# Output sequence counts for each of the files
+# and put them into seperate text file in the output folder
+echo "Extracting sequence counts from each of the files"
